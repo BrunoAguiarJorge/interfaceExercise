3bd9ba8e03a21bd910e94c458beea0ee8a83d508
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-09-20 18:07:08 +0100

    Interface implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
-public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Program  {
 
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter contract data");
+		
+		System.out.println("Enter Number: ");
+		int number = sc.nextInt();
+		
+		Date date = sdf.parse(sc.nextLine());
+		System.out.println("Return dd/MM/yyyy");
+		
+		System.out.println("Contract value: ");
+		double contractValue = sc.nextDouble();
+		System.out.println("");
+		System.out.println("Enter number of installment");
+		int inastallments = sc.nextInt();
+		System.out.println("Installtments");
+		System.out.println("25/07/2018");
+
+		
+		
+		
 	}
 
 }
